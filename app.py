e330f6e8e95aaa37a7d107f64f7ce49e3331b796
khaledxab <khaledxab@gmail.com>
2023-09-09 18:18:20 +0200

    add more details

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 from flask import Flask, render_template, request, jsonify
 from geopy.geocoders import Nominatim
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
-
-
-
 
 def coordinates_to_address(latitude, longitude):
     geolocator = Nominatim(user_agent="my_geocoder")
     location = geolocator.reverse((latitude, longitude), exactly_one=True)
+    
     if location:
-        return location.raw['display_name']
+        address_details = {
+            "address": location.raw['display_name'],
+            "road": location.raw.get('address', {}).get('road', ''),
+            "city": location.raw.get('address', {}).get('city', ''),
+            "state": location.raw.get('address', {}).get('state', ''),
+            "country": location.raw.get('address', {}).get('country', ''),
+            "postcode": location.raw.get('address', {}).get('postcode', ''),
+            "suburb": location.raw.get('address', {}).get('suburb', ''),
+            "county": location.raw.get('address', {}).get('county', ''),
+            "country_code": location.raw.get('address', {}).get('country_code', ''),
+        }
+        return address_details
     else:
-        return "Address not found"
-
+        return {"error": "Address not found"}
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -23,10 +30,9 @@ def index():
         data = request.json
         latitude = float(data["latitude"])
         longitude = float(data["longitude"])
-        address = coordinates_to_address(latitude, longitude)
-        return jsonify({"address": address})
+        address_details = coordinates_to_address(latitude, longitude)
+        return jsonify(address_details)
     return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
